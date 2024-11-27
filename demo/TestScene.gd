a10dd918503e5414e72d4c217289973419643eab
Erik Nilson <eriknielsen@users.noreply.github.com>
2024-11-27 09:21:10 +0100

    removed logs

--- demo/TestScene.gd
+++ demo/TestScene.gd
@@ -15,24 +15,19 @@ func _ready():
 func _input(event : InputEvent):
 	if event is InputEventMouseButton:
 		if event.pressed:
-			print("begin")
 			begin = event.position
 			end = event.position
 			draw_enabled = true
 		else:
 			draw_enabled = false
 			end = event.position
-			print("end")
 			# redraw
 			queue_redraw()
 			$Slicer2D.slice_world(begin, end)
 	elif event is InputEventMouseMotion and draw_enabled:
 		queue_redraw()
 		end = event.position
-		print("end with draw enabled")
 
 func _draw():
-	print(draw_enabled)
 	if draw_enabled:
-		print("drawing")
 		draw_line(begin, end, Color.RED)
